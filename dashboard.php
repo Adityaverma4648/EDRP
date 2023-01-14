87a04a48ae4e1ca44a87525ae512f5c52f1f6106
Aditya verma <87409406+Adityaverma4648@users.noreply.github.com>
2023-01-14 11:37:48 +0530

    making major changes

--- dashboard.php
+++ dashboard.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-    <!-- bootstrap cdn  -->
-           
-    <!-- Latest compiled and minified CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Latest compiled JavaScript -->
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Dashboard</title>
+     <!-- bootstrap cdn  -->
+
+     <!-- Latest compiled and minified CSS -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+     <!-- Latest compiled JavaScript -->
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
-    <!-- ends here--------------------------------------------------------------------------------------- -->
+     <!-- ends here--------------------------------------------------------------------------------------- -->
 
      <!-- importing google fonts  -->
      <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -32,175 +33,175 @@
 
      <!--  heat map  -->
 
-            <script src="/path/to/jquery.min.js"></script>
-            <script src="js/github_contribution.js"></script>
-            <link href="css/github_contribution_graph.css" rel="stylesheet" />
- 
+     <script src="/path/to/jquery.min.js"></script>
+     <script src="js/github_contribution.js"></script>
+     <link href="css/github_contribution_graph.css" rel="stylesheet" />
 
-<?php
-      include "conn.php";
-      include "session.php";
-?>
+
+     <?php
+     include "conn.php";
+     include "session.php";
+     ?>
 </head>
+
 <body>
-    <section id="ContainerDashboard" class="row">
+     <!-- <section id="ContainerDashboard" class="row">
 
-    <!-- navimcons -->
-          <div class="col-sm-1" id="navIcons">
-            <ul class="d-flex flex-column align-items -center justify-space-evenly">
-              <li class="d-flex flex-column align-items-center justify-content-center py-4" id="profile">
-                     <a href="#" class="text-decoration-none">
-                          <img src="#" alt="profile">
-                     </a>
-                     <span class="text-white">
-                     <!-- username -->
-                     <?php echo $login_session; ?>
-                     </span>
-                     <span class="text-white">
-                    <?php
-                         $sql =  "SELECT name,email FROM student WHERE userid = '$login_session' ";
-                         $result = $conn->query($sql);
-                         if ($result->num_rows > 0) {
-                              // output data of each row
-                              while($row = $result->fetch_assoc()) {
-                                echo  $row["name"] ;
-                              }
-                         }
-
-
-                    ?>
-                     </span>
-              </li>
-              <li class="py-4 elem">
-                     <a href="#" class="d-flex flex-column text-decoration-none">
-                          <i class="fa fa-home"></i>
-                          <span>
-                             Home
-                          </span>
-                     </a>
-              </li>
-              <li class="py-4 elem">
-                     <a href="attendance.php" class="d-flex flex-column text-decoration-none">
-                          <i class="fa fa-user-check"></i>
-                          <span>
-                             Attendance
-                          </span>
-                     </a>
-              </li>
-              <li class="py-4 elem">
-                     <a href="#" class="d-flex flex-column text-decoration-none">
-                          <i class="fa fa-money"></i>
-                          <span>
-                             Fees
-                          </span>
-                     </a>
-              </li>
-              <li class="py-4 elem">
-                     <a href="#" class="d-flex flex-column text-decoration-none">
-                          <i class="fa fa-group"></i>
-                          <span>
-                             Faculty
-                          </span>
-                     </a>
-              </li>
-              <li class="py-4 elem">
-                     <a href="#" class="d-flex flex-column text-decoration-none">
-                          <i class="fa fa-envelope"></i>
-                          <span>
-                             Feedback
-                          </span>
-                     </a>
-              </li>
-              <li class="py-4 elem">
-                     <a href="#" class="d-flex flex-column text-decoration-none">
-                          <i class="far fa-calendar"></i>
-                          <span >
-                             calendar
-                          </span>
-                     </a>
-              </li>
-              <li class="py-4 elem">
-                     <a href="logout.php" class="d-flex flex-column text-decoration-none">
-                          <i class="fa fa-gear"></i>
-                          <span>
-                             Settings
-                          </span>
-                     </a>
-              </li>
-              <li class="py-4 elem">
-                     <a href="logout.php" class="d-flex flex-column text-decoration-none">
-                          <i class="fa fa-sign-out"></i>
-                          <span>
-                             Logout
-                          </span>
-                     </a>
-              </li>
-              
-              
-            </ul>
-          </div>
+          <!-- navimcons -->
+     <!-- <div id="navIcons" style="width: 8vw">
+          <ul class="d-flex flex-column align-items -center justify-space-evenly">
+               <li class="d-flex flex-column align-items-center justify-content-center py-4" id="profile">
+                    <a href="#" class="text-decoration-none">
+                         <img src="#" alt="p">
+                    </a>
+                    <small class="text-white">
+                         <!-- username -->
+     <!-- <?php echo $login_session; ?>
+     </small>
+     <small class="text-white">
+          <?php
+          $sql =  "SELECT name,email FROM student WHERE userid = '$login_session' ";
+          $result = $conn->query($sql);
+          if ($result->num_rows > 0) {
+               // output data of each row
+               while ($row = $result->fetch_assoc()) {
+                    echo  $row["name"];
+               }
+          }
+          ?>
+     </small>
+     </li>
+     <li class="py-4 elem">
+          <a href="#" class="d-flex flex-column text-decoration-none">
+               <i class="fa fa-home"></i>
+               <small>
+                    Home
+               </small>
+          </a>
+     </li>
+     <li class="py-4 elem">
+          <a href="attendance.php" class="d-flex flex-column text-decoration-none">
+               <i class="fa fa-user-check"></i>
+               <small>
+                    Attendance
+               </small>
+          </a>
+     </li>
+     <li class="py-4 elem">
+          <a href="#" class="d-flex flex-column text-decoration-none">
+               <i class="fa fa-money"></i>
+               <span>
+                    Fees
+               </span>
+          </a>
+     </li>
+     <li class="py-4 elem">
+          <a href="#" class="d-flex flex-column text-decoration-none">
+               <i class="fa fa-group"></i>
+               <span>
+                    Faculty
+               </span>
+          </a>
+     </li>
+     <li class="py-4 elem">
+          <a href="#" class="d-flex flex-column text-decoration-none">
+               <i class="fa fa-envelope"></i>
+               <small>
+                    Feedback
+               </small>
+          </a>
+     </li>
+     <li class="py-4 elem">
+          <a href="#" class="d-flex flex-column text-decoration-none">
+               <i class="far fa-calendar"></i>
+               <span>
+                    calendar
+               </span>
+          </a>
+     </li>
+     <li class="py-4 elem">
+          <a href="logout.php" class="d-flex flex-column text-decoration-none">
+               <i class="fa fa-gear"></i>
+               <small>
+                    Settings
+               </small>
+          </a>
+     </li>
+     <li class="py-4 elem">
+          <a href="logout.php" class="d-flex flex-column text-decoration-none">
+               <i class="fa fa-sign-out"></i>
+               <small>
+                    Logout
+               </small>
+          </a>
+     </li>
 
 
-          <!-- main body  -->
-          <div class="col-sm-10 px-1 d-flex align-items-center justify-content-center" id="mainBody">
+     </ul>
+     </div> -->
+
+
+     <!-- main body  -->
+     <!-- <div class="col-sm-10 px-1 d-flex align-items-center justify-content-center" id="mainBody">
 
           <div class="cont row d-flex flex-col justify-content-center align-items-center">
-                  <div class="col-sm-11 my-2 text-light d-flex flex-column justify-content-center align-items-center" id="mainHeaderCont">
-                       <div class="d-flex px-5 align-items-center justify-content-end" style="width:100%;cursor:pointer;">
-                             <i class="fa fa-bell text-white" style="font-size:23px"></i>
-                       </div>
-                       <h3 class="d-flex justify-content-center align-items-center" style="height:70%;Width:100%;">Welcome! <?php  
-                       $sql =  "SELECT name,email FROM student WHERE userid = '$login_session' ";
-                         $result = $conn->query($sql);
-                         if ($result->num_rows > 0) {
-                              // output data of each row
-                              while($row = $result->fetch_assoc()) {
-                                echo  $row["name"] ;
-                              }
-                         }
-                          ?>   </h3>
-                  </div>
-                  <div class="col-sm-11" id="performance">
-                     <div class="row">
-                          <div class="col-sm-2 px-2" id="vertical">
-                               <div id="vertCont">
-                                         <div class="text-center text-white h5 py-2">
-                                               Performance 
-                                         </div>
-                                        
-                               </div>
-                          </div>
-                          <div class="col-sm-10" id="horizontal">
-                              <div class="row">
-                                    <div class="col-sm-12 my-1 d-flex justify-content-center ">
-                                         <span class="h5 container text-white py-2">
-                                                 Attendance
-                                         </span>
-                                    </div>
-                                    <div class="col-sm-12 my-1">
-                                         <div class="text-white h5 py-2 px-4">
-                                               Performance 
-                                         </div>
-                                         <canvas id="bar_chart" height="50px" width="50px"></canvas>
+               <div class="col-sm-11 my-2 text-light d-flex flex-column justify-content-center align-items-center" id="mainHeaderCont">
+                    <div class="d-flex px-5 align-items-center justify-content-end" style="width:100%;cursor:pointer;">
+                         <i class="fa fa-bell text-white" style="font-size:23px"></i>
+                    </div>
+                    <h3 class="d-flex justify-content-center align-items-center" style="height:70%;Width:100%;">Welcome! <?php
+                                                                                                                             $sql =  "SELECT name,email FROM student WHERE userid = '$login_session' ";
+                                                                                                                             $result = $conn->query($sql);
+                                                                                                                             if ($result->num_rows > 0) {
+                                                                                                                                  // output data of each row
+                                                                                                                                  while ($row = $result->fetch_assoc()) {
+                                                                                                                                       echo  $row["name"];
+                                                                                                                                  }
+                                                                                                                             }
+                                                                                                                             ?> </h3>
+               </div>
+               <div class="col-sm-11" id="performance">
+                    <div class="row">
+                         <div class="col-sm-2 px-2" id="vertical">
+                              <div id="vertCont">
+                                   <div class="text-center text-white h5 py-2">
+                                        Performance
+                                   </div>
 
-                                    </div>
                               </div>
-                          </div>
-                     </div>
-                  </div>
+                         </div>
+                         <div class="col-sm-10" id="horizontal">
+                              <div class="row">
+                                   <div class="col-sm-12 my-1 d-flex justify-content-center ">
+                                        <span class="h5 container text-white py-2">
+                                             Attendance
+                                        </span>
+                                   </div>
+                                   <div class="col-sm-12 my-1">
+                                        <div class="text-white h5 py-2 px-4">
+                                             Performance
+                                        </div>
+                                        <canvas id="bar_chart" height="50px" width="50px"></canvas>
+
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+               </div>
           </div>
 
-          </div>
+     </div> -->
 
-          <!-- activity log -->
-          <div class="col-sm-1" id="activityLog">
-            <ul class="d-flex flex-column align-items-center justify-space-evenly">
-                
-              
-            </ul>
-          </div>
-    </section>
-    
-    <script src="chart.js"></script>
+     <!-- activity log -->
+     <!-- <div class="col-sm-1" id="activityLog">
+          <ul class="d-flex flex-column align-items-center justify-space-evenly">
+
+
+          </ul>
+     </div> 
+     </section>-->
+
+     <!-- <script src="chart.js"></script> -->
 </body>
+
 </html>
